903a095b703e79b0d9bfb5a3d4e3ca24ce013b6b
rrepezza <rodolphorepezza@gmail.com>
2019-11-28 20:36:27 -0300

    Adição do certificado na api. https funcionando

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+const https = require("https");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./app/controllers/index')(app);
 
-app.listen(3000);
+const privateKey = fs.readFileSync(__dirname + '/key.pem');
+const privateCert = fs.readFileSync(__dirname + '/cert.pem');
+
+https.createServer({
+    key: privateKey,
+    cert: privateCert,
+    passphrase: 'agendex'
+}, app)
+.listen(3000);
+
+//app.listen(3000);
 
